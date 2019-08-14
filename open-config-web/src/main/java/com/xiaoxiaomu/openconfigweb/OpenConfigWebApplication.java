1a6966fbff1783617165925a2dadad1fecd2209b
wangyoucai <youcai@kuaishou.com>
2019-08-14 17:28:11 +0800

    first commit

---
+++ open-config-web/src/main/java/com/xiaoxiaomu/openconfigweb/OpenConfigWebApplication.java
@@ -0,0 +1,13 @@
+package com.xiaoxiaomu.openconfigweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenConfigWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenConfigWebApplication.class, args);
+    }
+
+}
